1a030083769dca3923592ff5abc311c0bd94027c
Dalton Claybrook <daltonclaybrook@gmail.com>
2019-03-21 22:32:09 -0400

    Hooked up redis

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,37 @@
 import { APIGatewayEvent, APIGatewayEventRequestContext, APIGatewayProxyResult } from 'aws-lambda';
+import redis from 'redis';
+import { promisify } from 'util';
 
-exports.handler = async (event: APIGatewayEvent, context: APIGatewayEventRequestContext): Promise<APIGatewayProxyResult> => {
-    const response = JSON.stringify(event);
+const client = redis.createClient({
+    host: process.env.REDIS_HOST,
+    port: parseInt(process.env.REDIS_PORT || '', 10),
+});
+
+const hget = promisify(client.hget).bind(client);
+const hset = promisify(client.hset).bind(client);
+
+const lambdaHandler = async (event?: APIGatewayEvent, context?: APIGatewayEventRequestContext): Promise<APIGatewayProxyResult> => {
+    console.log('handling...');
+    const existing = await hget('key1', 'field1');
+    if (existing) {
+        console.log(`key1 - field1: ${existing}`);
+    } else {
+        console.log('no existing');
+    }
+    const reply = await hset('key1', 'field1', 'fizzbuzz');
+    console.log(`no longer quitting redis: ${reply}`);
+
     return {
         statusCode: 200,
-        body: response,
+        body: existing,
     };
 };
+
+if (process.env.DEBUG) {
+    console.log('calling handler...');
+    lambdaHandler().then((result: APIGatewayProxyResult) => {
+        console.log(JSON.stringify(result));
+    });
+}
+
+exports.handler = lambdaHandler;
